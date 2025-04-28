9cb5bb26745f8f977f7d435ea6347eaacdcd07cc
Nino-FR-04 <davidfrx322@gmail.com>
2025-04-27 23:44:46 -0500

    Implementación - Metodos: Length,destryList y search

--- Trabajos_Individuales/FloreNino/LinkedList/ListaEnlazada.java
+++ Trabajos_Individuales/FloreNino/LinkedList/ListaEnlazada.java
@@ -8,8 +8,8 @@ public class ListaEnlazada <E extends Comparable<E>> implements Lista<E> {
      */
     
     private Node first;
-
-     //Constructor
+    
+    //Constructor
     public ListaEnlazada(){
         this.first = null;
     }
@@ -26,6 +26,58 @@ public class ListaEnlazada <E extends Comparable<E>> implements Lista<E> {
         return this.first == null;
     }
 
+    /**
+     * Calcula el tamaño de la lista.
+     * Se recorre desde el primer nodo y se cuenta cada nodo hasta llegar a uno nulo,
+     * indicando el final de la lista.
+     *
+     * @return cantidad de elementos en la lista.
+     */
+    @Override
+    public int length(){
+        int size = 0;
+        Node nodoActual = this.first;
+        
+        while(nodoActual != null){
+            nodoActual = nodoActual.next;
+            size++;
+        }
+
+        return size;
+    }
+
+    /**
+     * Elimina todos los elementos de la lista,
+     * estableciendo la referencia al primer nodo como nula.
+     */
+    @Override
+    public void destroyList(){
+        this.first = null;
+    }
+
+    /**
+     * Busca un elemento en la lista y devuelve su posición.
+     *
+     * @param data el elemento que se desea buscar.
+     * @return el índice donde se encuentra el elemento, o -1 si no se encuentra en la lista.
+     */
+    @Override
+    public int search(E data) {
+        if (this.isEmptyList()) return -1;
+    
+        int index = 0;
+        Node actual = this.first;
+    
+        while (actual != null) {
+            if (actual.data.compareTo(data) == 0) {
+                return index;
+            }
+            actual = actual.next;
+            index++;
+        }
+    
+        return -1; // Si no se encuentra el elemento
+    }
 
     /**
      * Clase interna que representa un nodo de la lista.
