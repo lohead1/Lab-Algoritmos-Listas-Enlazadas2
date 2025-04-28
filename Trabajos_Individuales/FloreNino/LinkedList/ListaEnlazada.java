2c109a4585957ef463b8cff42a56fbb7cae6896f
Nino-FR-04 <davidfrx322@gmail.com>
2025-04-27 23:34:35 -0500

    Implementación de la Clase interna Node<E> y comentada

--- Trabajos_Individuales/FloreNino/LinkedList/ListaEnlazada.java
+++ Trabajos_Individuales/FloreNino/LinkedList/ListaEnlazada.java
@@ -1,5 +1,32 @@
 package LinkedList;
 
-public class ListaEnlazada {
+public class ListaEnlazada <E extends Comparable<E>> implements Lista<E> {
     
+
+
+
+    /**
+     * Clase interna que representa un nodo de la lista.
+     * Cada nodo almacena un dato genérico @param <E> y una referencia al siguiente nodo.
+     *
+     */
+    private class Node {
+        /** Valor genérico a guardar en el nodo. */
+        private E data;
+
+        /** Referencia al siguiente nodo en la lista. */
+        private Node next;
+
+        /**
+         * Crea un nuevo nodo con el dato especificado.
+         * La referencia al siguiente nodo se inicializa como null.
+         *
+         * @param data el dato que se almacenará en el nodo.
+         */
+        public Node(E data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
 }
